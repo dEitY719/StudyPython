d1bc06e165eabae354c2a407624134d8f37d47a9
ByoungwooYoon <deity719@naver.com>
2023-07-08 03:04:01 +0900

    [Test] rsplit argument

--- convert_file_extension.py
+++ convert_file_extension.py
@@ -12,3 +12,13 @@ for filename in source_file_list:
     converted_file_list.append(new_filename)
 
 print(converted_file_list)
+
+file = 'AAA.c.o'
+file1 = file.split('.',1)[0]
+file2 = file.split('.',2)[0]
+file3 = file.split('.',3)[0]
+print(file1, file2, file3)
+rfile1 = file.rsplit('.',1)[0]
+rfile2 = file.rsplit('.',2)[0]
+rfile3 = file.rsplit('.',3)[0]
+print(rfile1, rfile2, rfile3)
